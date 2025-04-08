166a1fc7e03b20210bb01d5b3bea0f43fd022023
winniemwai <winniemwai5451@gmail.com>
2025-04-08 14:51:24 +0300

    added a top button and improved my CSS

--- src/index.js
+++ src/index.js
@@ -66,6 +66,20 @@ searchBtn.addEventListener("click", () => {
   currentQuery = input.length ? input : "cybercrime";
   fetchNews(currentQuery);
 });
+//back to top button
+const backToTopBtn = document.getElementById("back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
 
 // Initial load
 fetchNews(currentQuery);
